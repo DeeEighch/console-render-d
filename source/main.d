d5b889d3ee6f7cb7c82399ccfa431eaa519d34a1
Vladyslav Komarov <vladyslav.komarov@gmail.com>
2017-10-07 15:27:42 +0300

    split run to client and server

--- source/main.d
+++ source/main.d
@@ -1,16 +1,35 @@
 #!/usr/bin/rdmd
 import std.stdio;
+import std.getopt;
 import engine;
 
+enum RunConfig {
+    server,
+    client
+};
 
+RunConfig runConfig = RunConfig.client; 
 
 
 void main(string[] args)
 {
+    auto helpInformation = getopt(
+        args,
+        config.caseInsensitive,
+        "run|r", "[ client | server ]", &runConfig);
 
-    auto client = new Client();
-    
-    client.run(args);
-        
+    if(helpInformation.helpWanted){
+        defaultGetoptPrinter("Tanks v0.1", helpInformation.options);
+    }    
+
+
+    if(runConfig == RunConfig.client) {
+        auto client = new Client();
+        client.run(args);
+    } else {
+        writefln("not implemented!");
+    }
+
+            
 
 }
